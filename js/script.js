2dd26a7dcfa3eab9920d6f550bb1f64c839b74f5
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2021-08-04 12:45:27 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-
+// Query Selector
 var form = document.querySelector('#card');
 var searchBtn = document.querySelector('#searchBtn');
 var searchUser = document.querySelector('#searchUser');
 
 var ui = new UI();
 
+// Add Event Listener
 form.addEventListener('submit', (e) => {
     let userText = searchUser.value;
 
@@ -14,19 +15,15 @@ form.addEventListener('submit', (e) => {
             .then(data => {
                 if(data.message === 'Not Found') {
                     // show alert
-                    ui.showAlert('alert alert-danger');
+                    ui.showAlert('User not found','alert alert-danger');
                 } else {
                     // show profile
                     console.log(data);
                     ui.showProfile(data);
                 }
+               )}
+             }else {
+               ui.clearProfile();
+            }
+            e.preventDefault();
             })
-            .catch(err => {
-                console.log(err);
-            })
-    } else {
-        ui.clearProfile();
-    }
-
-    e.preventDefault();
-});
